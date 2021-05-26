ab1ccc328446caaf09ecfdb9aa6ed16a474d5f75
Andres Järviste <andres.jarviste@gmail.com>
2021-05-26 13:27:55 +0300

    proovimuudatused

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+function aeg(req, res) {
+  const aeg = new Date()
+  res.send("Kellaaeg on: " + aeg.toString())
+}
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/aeg', aeg)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
